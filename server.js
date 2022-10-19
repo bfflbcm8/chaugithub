a071c0beca0859968c07a8037b0e635d9a2fdec4
bfflbcm8 <113405317+bfflbcm8@users.noreply.github.com>
2022-10-19 21:24:58 +0700

    update complete fix ssh

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
